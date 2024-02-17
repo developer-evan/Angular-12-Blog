5894b15c8269b2f224a8a3b5e7970f7c9657c5e5
Mogeni <evansmogeni254@gmail.com>
2024-02-17 11:07:59 +0300

    first commitment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'blog';
+  data = 'Angular 12 Blog App';
+  getValues(val: string) {
+    // return 'Angular 12 Blog Application';
+    // alert(name);
+    console.warn(val);
+  }
+  num = 10;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UserAuthModule} from './user-auth/user-auth.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
